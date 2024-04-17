807409b7a06f0f46f695d1ee9ed15065be840b6e
ahnaf daiyan <bartamitdaiyan@gmail.com>
2024-04-17 15:57:27 +0600

    environment variables for Firebase config keys

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -2,13 +2,14 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCVyp1pziqxlwYV0qrd-ijbT_4birgU6So",
-  authDomain: "m9-a9-realestate.firebaseapp.com",
-  projectId: "m9-a9-realestate",
-  storageBucket: "m9-a9-realestate.appspot.com",
-  messagingSenderId: "518285279091",
-  appId: "1:518285279091:web:a2b5e444b3683865868f06"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId
 };
 
 // Initialize Firebase
