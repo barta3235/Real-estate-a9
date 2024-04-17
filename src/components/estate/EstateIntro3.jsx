33af444d3b04146196a99f6916f79ba2064facbf
ahnaf daiyan <bartamitdaiyan@gmail.com>
2024-04-17 15:42:54 +0600

    responsive alert

--- src/components/estate/EstateIntro3.jsx
+++ src/components/estate/EstateIntro3.jsx
@@ -3,8 +3,8 @@ import 'animate.css'
 
 const EstateIntro3 = () => {
     return (
-        <div className="my-[15px] md:mx-[10px] flex justify-end md:justify-center animate__slideInRight animate__animated"  style={{backgroundImage: `url(${wave3})`}}>
-                 <h1 className="py-[40px] px-[10px] md:px-[0px] text-[30px] font-bold text-white text-end">Our Agents</h1>
+        <div className="my-[15px] md:mx-[10px] flex justify-center md:justify-center animate__slideInRight animate__animated"  style={{backgroundImage: `url(${wave3})`}}>
+                 <h1 className="py-[40px] px-[10px] md:px-[0px] text-[30px] font-bold text-white">Our Agents</h1>
             </div>
     );
 };
