aa78607e65ae01bc564e4b720713f7ce68c267aa
ahnaf daiyan <bartamitdaiyan@gmail.com>
2024-04-18 14:24:50 +0600

    Responsive on update and user profile

--- src/components/pages/UpdateProfile.jsx
+++ src/components/pages/UpdateProfile.jsx
@@ -4,10 +4,14 @@ import { useContext } from 'react';
 import { AuthContext } from '../../provider/AuthProvider';
 import { Helmet } from 'react-helmet-async';
 import Swal from 'sweetalert2'
+import { IoMdPerson } from "react-icons/io";
+import { MdEmail } from "react-icons/md";
+import { IoMdPhotos } from "react-icons/io";
 
 const UpdateProfile = () => {
 
-    const {user,reworkProfile}=useContext(AuthContext);
+    const { user, reworkProfile } = useContext(AuthContext);
+    const {displayName,email,photoURL}=user
 
 
     const {
@@ -19,34 +23,40 @@ const UpdateProfile = () => {
     console.log(user)
 
     const onSubmit = (data) => {
-        const {name,photoURL}=data;
+        const { name, photoURL } = data;
 
-        reworkProfile(name,photoURL)
-        .then((result)=>{
-            Swal.fire({
-                title: 'Success!',
-                text: 'Please reload the webpage',
-                icon: 'success',
-                confirmButtonText: 'Continue',
-                confirmButtonColor: '#8b0000'
-              })
-            console.log(result.user)
-        })
-        .catch((error)=>{
-            console.log(error.message)
-        })
+        reworkProfile(name, photoURL)
+            .then((result) => {
+                Swal.fire({
+                    title: 'Success!',
+                    text: 'Please reload the webpage',
+                    icon: 'success',
+                    confirmButtonText: 'Continue',
+                    confirmButtonColor: '#8b0000'
+                })
+                console.log(result.user)
+            })
+            .catch((error) => {
+                console.log(error.message)
+            })
 
-        
+
     }
     return (
-        <div className="hero min-h-screen bg-base-200" style={{backgroundImage: `url(https://i.ibb.co/sPKRN0g/wave2.png)`}}>
+        <div className="hero min-h-screen bg-base-200 lg:pl-[20px]" style={{ backgroundImage: `url(https://i.ibb.co/KG3C1hn/wave1.png)` }}>
             <Helmet>
-                 <title>South Stay | Update Profile</title>
+                <title>South Stay | Update Profile</title>
             </Helmet>
             <div className="hero-content flex-col lg:flex-row-reverse p-0 m-0">
-                <div className="text-center lg:text-left animate__rotateInDownLeft animate__animated">
-                    <h1 className="text-5xl font-bold text-red-800">Update your information!</h1>
-                    <p className="py-6 text-[18px] font-medium">We're here to assist you by keeping our information up to date, ensuring your convenience is our top priority. In return, we appreciate your help by providing the latest listings for both houses on sale and for rent.</p>
+                <div className="md:flex md:flex-col md:items-center lg:block">
+                    <div className="text-center lg:text-left animate__rotateInDownLeft animate__animated">
+                        <h1 className="text-5xl font-bold text-red-800">Update your information!</h1>
+                        <p className="py-6 text-[18px] font-medium">We're here to assist you by keeping our information up to date, ensuring your convenience is our top priority. In return, we appreciate your help by providing the latest listings for both houses on sale and for rent.</p>
+                    </div>
+                    <div className="border p-2 rounded-xl shadow-xl animate__rotateInDownLeft animate__animated md:w-[500px]">
+                        <h1 className=" font-medium text-[16px] md:text-[20px] flex items-center gap-[5px] w-full"><IoMdPerson className="text-2xl"></IoMdPerson><span className="font-semibold">User name: </span><span className="font-medium text-black bg-white rounded-lg p-1"> {displayName}</span></h1>
+                        <h1 className="mt-5 font-medium text-[16px] md:text-[20px] flex items-center gap-[5px]"><IoMdPhotos className="text-2xl"></IoMdPhotos><span className="font-medium">PhotoURL: </span><span className="font-medium overflow-x-scroll w-[150px] md:w-full text-black bg-white rounded-lg p-1"> {photoURL}</span></h1>
+                    </div>
                 </div>
                 <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100 animate__rotateInDownRight animate__animated">
                     <form onSubmit={handleSubmit(onSubmit)} className="card-body p-3 md:p-[32px]">
@@ -75,11 +85,11 @@ const UpdateProfile = () => {
                             <button className="btn bg-red-700 text-white hover:bg-red-900">Update</button>
                         </div>
                     </form>
-                    
+
                 </div>
 
             </div>
-            
+
         </div>
     );
 };
--- src/components/pages/UserProfile.jsx
+++ src/components/pages/UserProfile.jsx
@@ -1,29 +1,34 @@
 import { useContext } from "react";
 import { AuthContext } from "../../provider/AuthProvider";
 import { Helmet } from "react-helmet-async";
+import { IoMdPerson } from "react-icons/io";
+import { MdEmail } from "react-icons/md";
+import { IoMdPhotos } from "react-icons/io";
 
 
 // https://i.ibb.co/sPKRN0g/wave2.png
 
 const UserProfile = () => {
 
-    const {user}=useContext(AuthContext);
-    const {displayName,email,photoURL}=user
+    const { user } = useContext(AuthContext);
+    const { displayName, email, photoURL } = user
 
     return (
-        <div className="hero min-h-screen bg-base-200" style={{backgroundImage: `url(https://i.ibb.co/sPKRN0g/wave2.png)`}}>
+        <div className="hero min-h-screen bg-base-200" style={{ backgroundImage: `url(https://i.ibb.co/sPKRN0g/wave2.png)` }}>
             <Helmet>
-                 <title>South Stay | User Profile</title>
+                <title>South Stay | User Profile</title>
             </Helmet>
-            <div className="hero-content flex-col md:flex-row p-0">
+            <div className="hero-content flex-col md:flex-col lg:flex-row p-0">
                 <img src={photoURL} className="animate__zoomInUp animate__animated md:w-[300px] rounded-full shadow-2xl mb-[20px] md:mb-[20px]" />
-                <form className="md:ml-[30px] animate__zoomInDown animate__animated">
-                    <h1 className="text-3xl md:text-5xl font-bold ">Hello, <span className="text-red-700">{displayName}</span></h1>
-                    <p className="text-[20px] font-medium mb-7"></p>
-                    <div className="border p-2 rounded-xl bg-gradient-to-r from-red-800 to-red-500 text-white w-full">
-                      <h1 className=" font-medium text-[16px] md:text-[20px]"><span className="font-bold">User name: </span>{displayName}</h1>
-                      <h1 className="mt-5 font-medium text-[16px] md:text-[20px]"><span className="font-bold">Email: </span>{email}</h1>
-                      <h1 className="mt-5 font-medium text-[16px] md:text-[20px] w-[200px]"><span className="font-bold">PhotoURL: </span><span className="whitespace-normal break-words">{photoURL}</span></h1>
+                <form className="md:ml-[30px] animate__zoomInDown animate__animated md:flex md:flex-col md:items-center lg:block">
+                    <h1 className="text-3xl md:text-5xl font-bold mb-[20px] md:text-center lg:text-start">Hello, <span className="text-red-700">{displayName? displayName : 'User'}</span></h1>
+                    <div className="border p-2 rounded-xl bg-gradient-to-r from-red-800 to-red-500 text-white md:w-[500px]">
+                        <h1 className=" font-medium text-[16px] md:text-[20px] flex items-center gap-[5px] w-full"><IoMdPerson className="text-2xl"></IoMdPerson><span className="font-semibold">User name: </span><span className=" text-black bg-white rounded-lg p-1"> {displayName? displayName : 'User'}</span></h1>
+                        <h1 className="mt-5 font-medium text-[16px] md:text-[20px] flex items-center gap-[5px] w-full"><MdEmail className="text-2xl"></MdEmail><span className="font-bold">Email: </span><span className=" text-black bg-white rounded-lg p-1"> {email ? email : 'No email found'}</span></h1>
+                        <div className="mt-5 font-medium text-[16px] md:text-[20px] flex items-center gap-[5px] w-full">
+                            <div className="flex gap-[5px]"><IoMdPhotos className="text-2xl"></IoMdPhotos><h1>PhotoURL: </h1></div>
+                            <div className="overflow-x-scroll w-[200px] md:w-full text-black bg-white rounded-lg p-1 border-[5px]"> {photoURL}</div>
+                        </div>
                     </div>
                 </form>
             </div>
